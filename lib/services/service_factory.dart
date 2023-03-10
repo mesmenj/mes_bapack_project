d53c44f31003c435f93737fcb64d63d5d0a7da49
mesmenj <109738805+mesmenj@users.noreply.github.com>
2023-03-10 11:26:13 +0000

    second commit

---
+++ lib/services/service_factory.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ServiceFactory extends StatelessWidget {
+  final Widget child;
+  const ServiceFactory({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(providers: [], child: child);
+  }
+}
